376b1dfc0436b648e32460f9fb4eef503215289e
kurniawan <kurni.aawann19@gmail.com>
2025-05-04 21:52:50 +0700

    ADD logout button to clear user token on Home Page

--- lib/features/presentation/pages/home/home_page.dart
+++ lib/features/presentation/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_alumunium/features/presentation/getx/cache_local/cache.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(
+        title: const Text('Home'),
+        leading: IconButton(
+          onPressed: () {
+            TokenStorage.clearUserToken();
+          },
+          icon: Icon(Icons.logout),
+        ),
+      ),
       body: const Center(
         child: Text('Welcome to Home Page!'),
       ),
