96f0c30753da5f0d2daa01b850533e8ca8e5f680
kurniaawann <kurni.aawann19@gmail.com>
2025-05-09 00:26:29 +0700

    UPDATE HomeBody: replace custom item card implementation with BuildItemCard widget and adjust item spacing

--- lib/features/presentation/pages/home/widgets/home_body.dart
+++ lib/features/presentation/pages/home/widgets/home_body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:mobile_alumunium/common/theme/app_colors.dart';
 import 'package:mobile_alumunium/features/domain/entities/home.dart';
-import 'package:mobile_alumunium/features/presentation/pages/home/widgets/backgourd_value.dart';
+import 'package:mobile_alumunium/features/presentation/pages/home/widgets/build_item_card.dart';
 import 'package:mobile_alumunium/features/presentation/pages/home/widgets/sliver_appbar.dart';
 import 'package:mobile_alumunium/features/presentation/pages/home/widgets/statistic_item.dart';
 
@@ -116,169 +115,18 @@ class HomeBody extends StatelessWidget {
             sliver: SliverList.separated(
               itemCount: home.item.length,
               separatorBuilder: (BuildContext context, int index) {
-                return SizedBox(height: 20);
+                return SizedBox(height: 30);
               },
               itemBuilder: (BuildContext context, int index) {
                 final item = home.item[index];
 
-                return _buildItemCard(context, item);
+                return BuildItemCard(
+                  item: item,
+                );
               },
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildItemCard(BuildContext context, ItemEntity item) {
-    final textTheme = Theme.of(context).textTheme;
-    final dateFormat = DateFormat('dd MMM yyyy');
-    final isLowStock = item.stock < 5;
-
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Text(
-                    item.itemName,
-                    style: textTheme.titleMedium?.copyWith(
-                      fontWeight: FontWeight.w600,
-                      color: isLowStock
-                          ? AppColors.errorColor
-                          : AppColors.primaryDarkColor,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: isLowStock
-                        ? AppColors.errorColor.withOpacity(0.1)
-                        : AppColors.primaryColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: isLowStock
-                          ? AppColors.errorColor.withOpacity(0.3)
-                          : AppColors.primaryColor.withOpacity(0.3),
-                      width: 1,
-                    ),
-                  ),
-                  child: Text(
-                    '${item.stock} stok',
-                    style: textTheme.labelMedium?.copyWith(
-                      color: isLowStock
-                          ? AppColors.errorColor
-                          : AppColors.primaryColor,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            if (item.itemCode != null) ...[
-              Row(
-                children: [
-                  Text(
-                    'Kode: ',
-                    style: textTheme.bodySmall?.copyWith(),
-                  ),
-                  BackgourdValue(
-                    value: item.itemCode.toString(),
-                  )
-                ],
-              ),
-            ],
-            if (item.height != null && item.width != null) ...[
-              const SizedBox(height: 4),
-              Row(
-                children: [
-                  Text(
-                    'Dimensi: ',
-                    style: textTheme.bodySmall?.copyWith(),
-                  ),
-                  BackgourdValue(
-                    value: '${item.height} x ${item.width}',
-                  )
-                ],
-              )
-            ],
-            const SizedBox(height: 12),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    RichText(
-                        text: TextSpan(
-                      text: 'Dibuat: ',
-                      style: textTheme.bodySmall?.copyWith(
-                        color: AppColors.primaryGreyColor,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: dateFormat.format(item.createdAt),
-                          style: textTheme.bodySmall?.copyWith(
-                              // color: AppColors.primaryColor,
-                              ),
-                        ),
-                      ],
-                    )),
-                    RichText(
-                        text: TextSpan(
-                      text: 'Diperbaharui: ',
-                      style: textTheme.bodySmall?.copyWith(
-                        color: AppColors.primaryGreyColor,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: dateFormat.format(item.createdAt),
-                          style: textTheme.bodySmall?.copyWith(
-                              // color: AppColors.primaryColor,
-                              ),
-                        ),
-                      ],
-                    )),
-
-                    // Row(
-                    //   children: [
-                    //     Text(
-                    //       'Ditambahkan: ',
-                    //       style: textTheme.bodySmall?.copyWith(),
-                    //     ),
-                    //     BackgourdValue(
-                    //       value: dateFormat.format(item.createdAt),
-                    //     )
-                    //   ],
-                    // ),
-                    // Row(
-                    //   children: [
-                    //     Text(
-                    //       'Diupdate: ',
-                    //       style: textTheme.bodySmall?.copyWith(),
-                    //     ),
-                    //     BackgourdValue(
-                    //       value: dateFormat.format(item.createdAt),
-                    //     )
-                    //   ],
-                    // ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
       ),
     );
   }
